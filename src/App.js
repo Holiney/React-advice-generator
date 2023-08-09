5e845515fbe58e5480f9595d56c8b0afe719f9a5
CodeSandbox <support@codesandbox.io>
2023-08-09 16:20:04 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+function Message(props) {
+  return (
+    <p>
+      Ви прочитали: <strong>{props.count}</strong>
+    </p>
+  );
+}
